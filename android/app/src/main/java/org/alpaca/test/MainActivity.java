6aa78b475069b023081b00b04a692d1199d22ef2
wangmuy <wangmuy@gmail.com>
2023-03-28 21:01:59 +0800

    mock switch

--- android/app/src/main/java/org/alpaca/test/MainActivity.java
+++ android/app/src/main/java/org/alpaca/test/MainActivity.java
@@ -2,6 +2,7 @@ package org.alpaca.test;
 
 
 import static org.alpaca.test.Const.PREFIX;
+import static org.alpaca.test.NativeUtils.StatusFailed;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,14 +26,21 @@ import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = PREFIX + "MainActivity";
-    private StringBuilder mSb = new StringBuilder();
 
-    private HandlerThread mHT = new HandlerThread("ModelThread");
+    private final StringBuilder mSb = new StringBuilder();
+    private final HandlerThread mHT = new HandlerThread("ModelThread");
 
     private static class ModelHandler extends Handler {
-        private static final boolean MOCK = true;
         public static final int MSG_LOAD_MODEL = 1;
         public static final int MSG_GET_ANSWER = 2;
+        public static final int MSG_SET_MOCK = 3;
+
+        private boolean mock = true;
+
+        public static class LoadModelObj {
+            TextView tv;
+            File file;
+        }
         public static class GetAnswerObj {
             String question;
             Consumer<String> answerCb;
@@ -48,24 +57,48 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(@NonNull Message msg) {
             switch (msg.what) {
-                case MSG_LOAD_MODEL:
-                    modelLoaded = NativeUtils.loadModel((File) msg.obj);
-                    Log.d(TAG, "loadModel ret=" + modelLoaded);
-                    break;
-                case MSG_GET_ANSWER:
+                case MSG_LOAD_MODEL: {
+                    LoadModelObj obj = (LoadModelObj) msg.obj;
+                    String str;
+                    if (mock) {
+                        str = "loadModel but set to mock, ignore";
+                    } else {
+                        if (!modelLoaded) {
+                            modelLoaded = NativeUtils.loadModel((File) obj.file);
+                            str = "loadModel ret=" + modelLoaded;
+                        } else {
+                            str = "loadModel already loaded";
+                        }
+                    }
+                    obj.tv.post(() -> {
+                        obj.tv.setText(str);
+                    });
+                    Log.d(TAG, str);
+                }
+                break;
+                case MSG_GET_ANSWER: {
                     GetAnswerObj obj = (GetAnswerObj) msg.obj;
                     String question = obj.question;
                     String answer;
-                    if (MOCK) {
+                    if (mock) {
                         answer = NativeUtils.getAnswerMock(obj.appContext, question, obj.cb);
                     } else {
+                        if (!modelLoaded) {
+                            obj.cb.onEmit("Please load model first.", StatusFailed);
+                            return;
+                        }
                         answer = NativeUtils.getAnswer(question, obj.cb);
                     }
                     Log.d(TAG, "getAnswer ret=" + answer);
                     if (obj.answerCb != null) {
                         obj.answerCb.accept(answer);
                     }
-                    break;
+                }
+                break;
+                case MSG_SET_MOCK: {
+                    mock = (boolean) msg.obj;
+                }
+                break;
             }
         }
     }
@@ -85,11 +118,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         TextView tv = findViewById(R.id.textview);
+
         Button btnLoadModel = findViewById(R.id.btnLoadModel);
         btnLoadModel.setOnClickListener(v -> {
             Message msg = Message.obtain();
             msg.what = ModelHandler.MSG_LOAD_MODEL;
-            msg.obj = new File(getFilesDir(), "ggml-alpaca-7b-q4.bin");
+            ModelHandler.LoadModelObj obj = new ModelHandler.LoadModelObj();
+            obj.tv = tv;
+            obj.file = new File(getFilesDir(), "ggml-alpaca-7b-q4.bin");
+            msg.obj = obj;
+            mModelHandler.sendMessage(msg);
+        });
+
+        SwitchCompat mockSwitch = findViewById(R.id.mockSwitch);
+        mockSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            Message msg = Message.obtain();
+            msg.what = ModelHandler.MSG_SET_MOCK;
+            msg.obj = isChecked;
             mModelHandler.sendMessage(msg);
         });
 
